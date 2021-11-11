4834d53da8221fd2fb45809a36a96658d05b2de7
antonellasannino <antonella.sannino001@studenti.uniparthenope.it>
2021-11-11 15:07:43 +0100

    prova

--- src/it/uniparthenope/informatica/prgriii9/l00/HelloWorld.java
+++ src/it/uniparthenope/informatica/prgriii9/l00/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Java World!");
+        System.out.println("Hello Java World!");
     }
 }
